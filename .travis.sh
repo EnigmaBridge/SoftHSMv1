a04238db3a066a224e19e279fb93e8640434a354
Dusan Klinec <dusan.klinec@gmail.com>
2016-04-19 23:18:41 +0200

    SH-3: travis; checks enabled again

--- .travis.sh
+++ .travis.sh
@@ -4,6 +4,8 @@
 
 sh autogen.sh && \
 ./configure --with-loglevel=4 --with-botan=$HOME/botan && \
-make all
+make all && \
+export LD_PRELOAD=$HOME/botan/lib/libbotan-1.10.so.1 && \
+make check
 
 #make all check
