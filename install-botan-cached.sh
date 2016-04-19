47bfdf17331087bef7f336a471f11164f299d976
Dusan Klinec <dusan.klinec@gmail.com>
2016-04-19 21:43:50 +0200

    SH-3: travis; obtain install script updated

--- install-botan-cached.sh
+++ install-botan-cached.sh
@@ -1,16 +1,15 @@
 !/bin/sh
 set -e
-if [ ! -f "$HOME/botan/botan" ]; then
+if [ ! -f "$HOME/botan/libbotan-1.10.so" ]; then
   wget http://botan.randombit.net/releases/Botan-1.10.12.tgz && \
   tar -xzvf Botan-1.10.12.tgz                                && \
   cd Botan-1.10.12                                           && \
   ./configure.py --prefix=$HOME/botan                        && \
   make                                                       && \
-  ./botan-test                                               && \
   make install                                               && \
-  echo -e "[\E[32m\033[1m OK \033[0m] Success"               || \
-  echo -e "[\E[31m\033[1mFAIL\033[0m] Update failed"         && \
+  echo -e "[\E[32m\033[1m OK \033[0m] Botan install Success" || \
+  echo -e "[\E[31m\033[1mFAIL\033[0m] Botan install Failed"  && \
   exit 1
 else
-  echo 'Using cached directory.';
+  echo 'Using cached Botan installation.';
 fi
